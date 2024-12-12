e44ac88886b17e7363ac4405d32ad53f690d8ec4
realsydrid <realsydrid@gmail.com>
2024-12-13 02:00:12 +0900

    로그인기능 구현

---
+++ login.js
@@ -0,0 +1,23 @@
+document.getElementById('loginBtn').addEventListener('click', async () => {
+    const userId = document.getElementById('id').value;
+    const userPassword = document.getElementById('password').value;
+
+    try {
+        const res = await fetch(`${userId}UserInfo.json`)
+
+        if (res.status === 404) {
+            alert('아이디가 존재하지 않습니다.')
+            return;
+        }
+        if (res.status === 200) {
+            const userData = await res.json()
+            if (userData.password === userPassword) {
+                window.location.href = `http://mark9815.dothome.co.kr/mybasket/${userId}MyInfo.html`;
+            } else {
+                alert('비밀번호가 일치하지 않습니다.');
+            }
+        } 
+    } catch (error) {        
+        console.error('에러');
+    }
+});
